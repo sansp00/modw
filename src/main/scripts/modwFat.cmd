cfb678a9e488d63dc032726469c854b1d3aa2ad6
Patrick Sansoucy <patrick.sansoucy@gmail.com>
2024-10-02 18:59:59 -0400

    Fix launch script

--- src/main/scripts/modwFat.cmd
+++ src/main/scripts/modwFat.cmd
@@ -30,4 +30,4 @@ goto error
 
 :init
 
-"%JAVA_HOME%\bin\java.exe" -jar %~dp0modw-@project.version@d project-pg.jar %*
+"%JAVA_HOME%\bin\java.exe" -jar %~dp0modw-@project.version@-jar-with-dependencies.jar %*

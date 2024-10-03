6c8fd5b8f0f3610615b0c48c319f029fbde5dcd8
Patrick Sansoucy <patrick.sansoucy@gmail.com>
2024-10-02 21:10:48 -0400

    Add modw debug launcher

---
+++ src/main/scripts/modwFatDbg.cmd
@@ -0,0 +1,33 @@
+@REM ----------------------------------------------------------------------------
+@REM Moderne CLI Wrapper startup batch script, version @project.version@
+@REM
+@REM Required ENV vars:
+@REM JAVA_HOME - location of a JDK home dir
+@REM ----------------------------------------------------------------------------
+
+
+@REM ==== JAVA_HOME VALIDATION ====
+if not "%JAVA_HOME%" == "" goto HasJavaHome
+
+echo. >&2
+echo Error: JAVA_HOME not found in your environment. >&2
+echo Please set the JAVA_HOME variable in your environment to match the >&2
+echo location of your Java installation. >&2
+echo. >&2
+goto error
+
+:HasJavaHome
+if exist "%JAVA_HOME%\bin\java.exe" goto init
+
+echo. >&2
+echo Error: JAVA_HOME is set to an invalid directory. >&2
+echo JAVA_HOME = "%JAVA_HOME%" >&2
+echo Please set the JAVA_HOME variable in your environment to match the >&2
+echo location of your Java installation. >&2
+echo. >&2
+goto error
+@REM ==== END JAVA_HOME VALIDATION ====
+
+:init
+
+"%JAVA_HOME%\bin\java.exe" -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5000 -jar %~dp0modw-@project.version@-jar-with-dependencies.jar %*

8330fd338a99addd58ca4523bc04371341b65265
Patrick Sansoucy <patrick.sansoucy@gmail.com>
2024-11-05 23:52:02 -0500

    Batch update

--- src/scripts/modw.cmd
+++ src/scripts/modw.cmd
@@ -7,20 +7,19 @@
 
 @echo off
 
-setlocal enabledelayedexpansion
 
 @REM ==== JAVA_HOME VALIDATION ====
-if not "%JAVA_HOME%" == "" goto HasJavaHome
+IF NOT "%JAVA_HOME%" == "" GOTO HasJavaHome
 
 echo. >&2
 echo Error: JAVA_HOME not found in your environment. >&2
 echo Please set the JAVA_HOME variable in your environment to match the >&2
 echo location of your Java installation. >&2
 echo. >&2
-goto error
+GOTO error
 
 :HasJavaHome
-if exist "%JAVA_HOME%\bin\java.exe" goto init
+IF exist "%JAVA_HOME%\bin\java.exe" GOTO ReadProperties
 
 echo. >&2
 echo Error: JAVA_HOME is set to an invalid directory. >&2
@@ -28,40 +27,54 @@ echo JAVA_HOME = "%JAVA_HOME%" >&2
 echo Please set the JAVA_HOME variable in your environment to match the >&2
 echo location of your Java installation. >&2
 echo. >&2
-goto error
+GOTO error
 @REM ==== END JAVA_HOME VALIDATION ====
 
-
-:init
-
+:ReadProperties
 set "MODW_HOME=%~dp0"
-set "MODW_USER_HOME=%MODW_HOME%\.modw"
+set "MODW_USER_HOME=%HOMEDRIVE%%HOMEPATH%\.modw"
+set "WRAPPER_REPO_PATH="
 
-:value
-set "key=%1"
-for /f "tokens=1,* delims==" %%a in ('findstr /r "^!key!=.*" !"%MODW_USER_HOME%/modw.properties"!') do (
-    set "value=%%b"
+IF not exist "%MODW_USER_HOME%\modw.properties" ( 
+	GOTO ExecuteWrapper
 )
-echo %value%
-goto :EOF
 
-set WRAPPER_JAR_PATH=%MODW_HOME%/modw-0.0.1-SNAPSHOT-pg.jar
-#set WRAPPER_JAR_PATH=%MODW_HOME%/modw-@project.version@-pg.jar
+CALL :getValueOf "wrapper.groupId"
+set "GROUPID=%value%"
 
-if exist %MODW_USER_HOME%\modw.properties" {
-	echo "Parsing 'modw.properties'"
-	set "GROUPID=call value "wrapper.groupId""
-	set "ARTIFACTID=call value "wrapper.artifactId""
-	set "VERSION=call value "wrapper.version""
-	set "QUALIFIER=call value "wrapper.qualifier""
+CALL :getValueOf "wrapper.artifactId"
+set "ARTIFACTID=%value%"
 
-	set "ARTIFACT_REPO_PATH=%GROUPID:.=\%"
-	set "WRAPPER_REPO_PATH="%MODW_USER_HOME%\repo\%ARTIFACT_REPO_PATH%\%ARTIFACTID%-%VERSION%-%QUALIFIER%.jar""
-	echo "Looking for '%WRAPPER_REPO_PATH%'"  	
+CALL :getValueOf "wrapper.version"
+set "VERSION=%value%"
 
-	if exist %WRAPPER_REPO_PATH% {
-		set "WRAPPER_JAR_PATH=%WRAPPER_REPO_PATH%"
-	}
-}
-echo "Running with wrapper jar '%WRAPPER_JAR_PATH%'"
-"%JAVA_HOME%\bin\java.exe" -jar %WRAPPER_JAR_PATH% %*
+CALL :getValueOf "wrapper.qualifier"
+set "QUALIFIER=%value%"
+
+set "ARTIFACT_REPO_PATH=%GROUPID:.=/%"
+set "WRAPPER_REPO_PATH=%MODW_USER_HOME%\repo\%ARTIFACT_REPO_PATH%\%ARTIFACTID%-%VERSION%-%QUALIFIER%.jar"
+
+:ExecuteWrapper
+if exist "%WRAPPER_REPO_PATH%" (
+  set "WRAPPER_JAR_PATH=%WRAPPER_REPO_PATH%"
+) else (
+  set "WRAPPER_JAR_PATH=%MODW_HOME%modw-0.0.1-SNAPSHOT-pg.jar"
+)
+
+echo "Running with wrapper %WRAPPER_JAR_PATH%" >&2
+%JAVA_HOME%\bin\java.exe -jar "%WRAPPER_JAR_PATH%" %*
+
+:getValueOf
+setlocal enabledelayedexpansion
+set "key=%~1"
+set "modwProperties=%MODW_USER_HOME%\modw.properties"
+FOR /f "tokens=1,2 delims==" %%a IN (!modwProperties!) DO (
+    IF "%%a"=="!key!" (
+        set "value=%%b"
+		endlocal
+		exit /b
+    )
+)
+set "value="
+endlocal
+exit /b 
